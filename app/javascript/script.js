006b1eaba75bdb0a3331062ee06f7068b5d084d7
Ryota Tomiyama <lit.karu.noir@gmail.com>
2021-03-17 23:17:24 +0900

    feat: enable jquery

---
+++ app/javascript/script.js
@@ -0,0 +1,20 @@
+$(function () {
+  var pageTop = $("#page-top");
+  pageTop.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      pageTop.fadeIn();
+    } else {
+      pageTop.fadeOut();
+    }
+  });
+  pageTop.click(function () {
+    $("body, html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+    return false;
+  });
+});
